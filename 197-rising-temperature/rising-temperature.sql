ea55262431e4d9a8b1d860b301550780f8fca1fe
Krriteish91 <riteshkr0346@gmail.com>
2026-07-27 19:29:22 +0530

    Time: 442 ms (78.82%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -2,4 +2,4 @@ SELECT a.id
 FROM Weather as a
 LEFT JOIN Weather as b
 ON DATE_SUB(a.recordDate, INTERVAL 1 DAY) = b.recordDate
-WHERE b.temperature < a.temperature ;
+where b.temperature < a.temperature
